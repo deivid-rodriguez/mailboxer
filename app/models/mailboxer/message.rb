dd050b91e37175f4e8b329865233632d63e35f2c
David Rodríguez <deivid.rodriguez@gmail.com>
2016-07-26 19:15:27 -0300

    Save object in a more straight way
    It's simpler to just save the parent and let the children be autosaved
    since this is how Rails works out of the box. We just need to make sure
    the receipts are properly tied to the parent notification. We get that,
    again, by using Rails standard constructs like `association#build`
    instead of the ad-hoc `build_receipt` method.

--- app/models/mailboxer/message.rb
+++ app/models/mailboxer/message.rb
@@ -26,16 +26,17 @@ class Mailboxer::Message < Mailboxer::Notification
     self.clean if should_clean
 
     #Receiver receipts
-    receiver_receipts = recipients.map { |r| build_receipt(r, 'inbox') }
+    receiver_receipts = recipients.map do |r|
+      receipts.build(receiver: r, mailbox_type: 'inbox', is_read: false)
+    end
 
     #Sender receipt
-    sender_receipt = build_receipt(sender, 'sentbox', true)
+    sender_receipt =
+      receipts.build(receiver: sender, mailbox_type: 'sentbox', is_read: true)
 
-    temp_receipts = [sender_receipt] + receiver_receipts
-
-    if temp_receipts.all?(&:valid?)
-      temp_receipts.each(&:save!)
+    if valid?
       Mailboxer::MailDispatcher.new(self, receiver_receipts).call
+      save!
 
       conversation.touch if reply
 
